d04c336c207bb4b0f9d63091706fe1319ce00138
tata-v <xhrl523@naver.com>
2023-07-11 14:17:05 +0900

    Time: 57 ms (68.50%), Space: 42.8 MB (18.87%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+  let arr = matrix;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      // [arr[i][j], arr[j][i]] = [arr[j][i], arr[i][j]];
+      [arr[i][j], arr[j][i]] = [arr[j][i], arr[i][j]];
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].reverse();
+  }
+
+  return arr;
+};
